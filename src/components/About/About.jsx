aab4b53e4db88d81da8bdc129edcf62f045e2ee2
Sandoval0315 <20200300@ricaldone.edu.sv>
2025-09-13 16:22:08 -0600

    changes

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";  
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about__container">
+      <h1>About Us</h1>
+      </div>
+    </div>
+  );
+};
+
+export default About;
